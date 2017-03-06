e2d78a217ea1456d52ceb10786153a8445116aae
yamadadesu <kakiharasou+yamada@gmail.com>
2017-03-07 01:42:43 +0900

    test

---
+++ strategy.rb
@@ -0,0 +1,37 @@
+class Report
+    attr_reader :title, :text
+    attr_accessor :formatter
+
+    def initialize(formatter)
+        @title     = "月次報告"
+        @text      = ["順調", "最高"]
+        @formatter = formatter
+    end
+
+    def output_report()
+        @formatter.output_report(self)
+    end
+end
+
+class HTMLFormatter
+    def output_report(context)
+        puts self.class.to_s
+    end
+end
+
+class PlainTextFormatter
+    def output_report(context)
+        puts self.class.to_s
+    end
+end
+
+report = Report.new(HTMLFormatter.new)
+report.output_report
+
+report.formatter = PlainTextFormatter.new
+report.output_report
+
+
+
+
+
